e636947408b7182a499b03caaec82d92b8d6ff9a
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2021-02-13 21:14:56 +0100

    refactor: add css for mobile contact section

--- #src/js/emailValidation.js
+++ #src/js/emailValidation.js
@@ -4,5 +4,6 @@ inputEmail.oninvalid = invalid;
 function invalid(event) {
     error.removeAttribute('hidden');
     inputEmail.classList ='error';
+    inputEmail.style.backgroundImage = 'url(../../img/contactSection/icon-error.svg)';
     event.preventDefault();
   }
--- VoxLoud/js/script.js
+++ VoxLoud/js/script.js
@@ -59,6 +59,7 @@ inputEmail.oninvalid = invalid;
 function invalid(event) {
   error.removeAttribute('hidden');
   inputEmail.classList = 'error';
+  inputEmail.style.backgroundImage = 'url(../../img/contactSection/icon-error.svg)';
   event.preventDefault();
 }
 
