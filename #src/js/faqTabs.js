991cec6e9d15192d38a2a14707ad5196684bf067
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2021-02-18 14:45:30 +0100

    fix: hover effects

--- #src/js/faqTabs.js
+++ #src/js/faqTabs.js
@@ -1,14 +1,26 @@
 const openQuestion = (evt, question) => {
     let i, tabAnswer, tabQuestion;
     tabAnswer = document.getElementsByClassName("tabAnswer");
-    for (i = 0; i < tabAnswer.length; i++) {
-      tabAnswer[i].style.display = "none";
-    }
     tabQuestion = document.getElementsByClassName("tabQuestion");
-    for (i = 0; i < tabQuestion.length; i++) {
-      tabQuestion[i].className = tabQuestion[i].className.replace(" active", "");
-    }
-    document.getElementById(question).style.display = "block";
-    evt.currentTarget.className += " active";
-};
 
+    // if(document.getElementById(question).style.display == "block") {
+    //   console.log('true')
+    //   for (i = 0; i < tabQuestion.length; i++) {
+    //     tabQuestion[i].classList.remove(' active');
+    //   }
+    // }
+    // for (i = 0; i < tabAnswer.length; i++) {
+    //   tabAnswer[i].style.display = "none";
+    // }
+
+    // for (i = 0; i < tabQuestion.length; i++) {
+    //   tabQuestion[i].className = tabQuestion[i].className.replace(" active", "");
+    // }
+    // document.getElementById(question).style.display = "block";
+    // evt.currentTarget.className += " active";
+    
+    console.log(evt, question)
+
+
+
+}
